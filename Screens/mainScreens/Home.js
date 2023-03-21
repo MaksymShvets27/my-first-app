8e28a7dd9e4efa84ab022cbad5718929e6a4e9d2
Maksym Shvets <maxim27061993@outlook.com>
2023-03-21 21:22:34 +0100

    hw_04

---
+++ Screens/mainScreens/Home.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import PostsScreen from "./PostsScreen";
+import CreatePostsScreen from "./CreatePostsScreen";
+import ProfileScreen from "./ProfileScreen";
+import { Ionicons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+const MainTab = createBottomTabNavigator();
+
+export default Home = ({ navigation: { navigate } }) => {
+  return (
+    <MainTab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+      }}
+    >
+      <MainTab.Screen
+        options={{
+          title: "Публикации",
+          headerTitleAlign: "center",
+          tabBarActiveTintColor: null,
+          headerRight: () => (
+            <Ionicons
+              onPress={() => navigate("Login")}
+              name="exit-outline"
+              size={24}
+              color="#BDBDBD"
+              style={{ marginRight: 16 }}
+            />
+            // <Button title="exit" onPress={() => navigate("Login")} />
+          ),
+          tabBarIcon: ({ focused, size, color }) => {
+            return <Ionicons name="grid-outline" size={24} color="black" />;
+          },
+        }}
+        name="Posts"
+        component={PostsScreen}
+      />
+      <MainTab.Screen
+        options={{
+          title: "Создать публикацию",
+          headerTitleAlign: "center",
+          tabBarActiveTintColor: null,
+          tabBarIcon: ({ focused, size, color }) => {
+            return (
+              <Ionicons
+                name="add"
+                size={size}
+                color="white"
+                style={{
+                  backgroundColor: "orange",
+                  width: 75,
+                  height: 40,
+                  borderRadius: 20,
+                  textAlign: "center",
+                  textAlignVertical: "center",
+                }}
+              />
+            );
+          },
+        }}
+        name="CreatePosts"
+        component={CreatePostsScreen}
+      />
+      <MainTab.Screen
+        options={{
+          headerShown: false,
+          tabBarActiveTintColor: null,
+          tabBarIcon: ({ focused, size, color }) => {
+            return <AntDesign name="user" size={24} color="black" />;
+          },
+        }}
+        name="Profile"
+        component={ProfileScreen}
+      />
+    </MainTab.Navigator>
+  );
+};
